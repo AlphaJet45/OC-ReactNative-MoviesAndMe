99dfa2199bc4d4d15afd8e126791d50b5734577c
Damien <damien.m.45@gmail.com>
2019-03-02 15:51:32 +0100

    Add external style with the API StyleSheet

--- components/search.js
+++ components/search.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import { View, TextInput, Button } from 'react-native'
+import { StyleSheet, View, TextInput, Button } from 'react-native'
 
 class Search extends React.Component {
     render () {
         return (
-            <View>
-                <TextInput placeholder="Titre du film"></TextInput>
-                <Button title="Rechercher" onPress={() => {}}></Button>
+            <View style = { styles.view }>
+                <TextInput placeholder="Titre du film" style = { styles.textinput }></TextInput>
+                <Button title="Rechercher" onPress={() => {}} style = { styles.button }></Button>
                 {/* Equivalent de : onPress = {function() {}} */}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    view: {
+        marginTop: 30
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5
+      },
+    button: {
+        height: 50
+    }
+})
 
 export default Search
